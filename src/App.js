9f8df08a74d1636c0fab7aaf7265a23af7809630
PriyanshuSha <priyanshusharma90284@gmail.com>
2024-02-01 11:14:26 +0530

    d

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import CreatePost from "./pages/CreatePost";
+import { routePath } from "./routes/route";
+import AllPosts from "./pages/AllPosts";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path={routePath.home} element={<Home />} />
+        <Route path={routePath.create} element={<CreatePost />} />
+        <Route path={routePath.posts} element={<AllPosts />} />
+      </Routes>
+    </Router>
   );
 }
 
